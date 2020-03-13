bc843d719013b4b8e370d34a1695774d836b3d9e
AnugrahLestari <nugratar59@gmail.com>
2020-03-13 20:23:54 +0800

    Tugas Praktikum 3

---
+++ H071191059/Praktikum 3/Encapsulation/src/Encapsulation/Item.java
@@ -0,0 +1,43 @@
+package Encapsulation;
+
+class Item {
+    private String name;
+    private int price;
+    private String info;
+
+    public Item(String name, int price, String info) {
+        this.name = name;
+        this.price = price;
+        this.info = info;
+    }
+
+    public void descItem() {
+        System.out.println(getName());
+        System.out.println(getPrice());
+        System.out.println(getInfo());
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+}
