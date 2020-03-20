d666264ecb5f1a4c16a3b1920d29befd0ca93ad1
annisaputriaulia <54925797+annisaputriaulia@users.noreply.github.com>
2020-03-20 23:57:17 +0800

    Rectangle.java

--- H071191029/Praktikum-1/Rectangle.java
+++ H071191029/Praktikum-1/Rectangle.java
@@ -1,7 +1,18 @@
-class Rectangle{
-    double height;
-    double width;
-    double getArea(){
-        return height*width;
+class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle(){
+        length = 0;
+        width = 0;
+    }
+
+    public Rectangle (int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getArea(){
+        return length*width;
     }
 }
