27a4520ceb8b31197f3527fb57fab5be97baf7a5
annisaputriaulia <ap7901211@gmail.com>
2020-04-09 19:19:11 +0800

    Tugas Praktikum 4

---
+++ H071191029/Praktikum-4/Koord.java
@@ -0,0 +1,12 @@
+package nis3;
+
+public class Koord extends Organisasi {
+
+    public Koord(String name, String id, String major) {
+        super(name, id, major);
+    }
+
+    public void showKoord() {
+        System.out.println("KOORDINATOR BIDANG DAN STAFF");
+    }
+}
