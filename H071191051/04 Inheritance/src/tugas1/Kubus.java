6217b4f3a6ac09c65e6ce388b25722bfb4a0faa9
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2020-04-28 04:08:51 +0800

    added tugas praktikum 4

---
+++ H071191051/04 Inheritance/src/tugas1/Kubus.java
@@ -0,0 +1,19 @@
+package tugas1;
+
+class Kubus extends BangunRuang {
+    private double r;
+
+    public Kubus(double r) {
+        this.r = r;
+    }
+
+    @Override
+    public double getLuasPermukaan() {
+        return 6 * Math.pow(r, 2);
+    }
+
+    @Override
+    public double getVolume() {
+        return Math.pow(r, 3);
+    }
+}
---
+++ H071191051/04 Inheritance/src/tugas1/Persegi.java
@@ -0,0 +1,18 @@
+package tugas1;
+
+class Persegi extends BangunDatar {
+
+    public Persegi(double sisi) {
+        super(sisi);
+    }
+
+    @Override
+    public double getKeliling() {
+        return 4 * getSisi();
+    }
+
+    @Override
+    public double getLuas() {
+        return getSisi() * getSisi();
+    }
+}
